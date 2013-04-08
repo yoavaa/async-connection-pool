76ad7e22c1718f1501822c975c942a7192d99c99
Yoav Abrahami <yoav@wix.com>
2013-04-08 11:17:19 +0300

    updated the atomic locking mechanism to prevent multiple cycles between the read and write method

--- src/main/scala/com/wixpress/hoopoe/asyncjdbc/impl/ConnectionWorkerStatistics.scala
+++ src/main/scala/com/wixpress/hoopoe/asyncjdbc/impl/ConnectionWorkerStatistics.scala
@@ -2,9 +2,10 @@ package com.wixpress.hoopoe.asyncjdbc.impl
 
 import java.util.concurrent.atomic.AtomicReference
 import com.wixpress.hoopoe.asyncjdbc._
+import java.util.concurrent.locks.ReentrantLock
 
 /**
- * 
+ * Counter class that measures a ConnectionWorker load and overhead times
  * @author Yoav
  * @since 4/3/13
  */
@@ -15,6 +16,7 @@ class ConnectionWorkerMeter(val timeSource: () => Millis = () => System.currentT
   // non-thread-safe (no need to make it thread safe)
   private var lastWaitOnQueueStart: Millis = 0
   private var lastWaitOnQueueTime: Millis = 0
+  private val lock = new ReentrantLock
 
   // thread-safe
   def startTask() {
@@ -56,37 +58,81 @@ class ConnectionWorkerMeter(val timeSource: () => Millis = () => System.currentT
     ConnectionWorkerStatistics(workingTime,overheadTime,sleepingTime,currentCounter.errorCount)
   }
 
-
+  /**
+   * Gets a snapshot of the atomicCounters value and resets the counter
+   *
+   * the method uses an atomic with lock update pattern
+   * we attempt to update the atomicCounters variable.
+   * If we fail due to concurrency, we try again, but this time
+   * we lock to ensure no one interrupts and updates our atomicCounters in the middle.
+   * @return the snapshot
+   */
   private def snapshotCounter: Counters = {
     var currentCounter: Counters = null
     var newCounter: Counters = null
+    var attempt = 1
     do {
-      currentCounter = atomicCounters.get()
-      newCounter = Counters(state = currentCounter.state)
+      lockOnSubsequentAttempt(attempt)
+      try {
+        currentCounter = atomicCounters.get()
+        newCounter = Counters(state = currentCounter.state)
+      }
+      finally {
+        releaseOnSubsequentAttempt(attempt)
+        attempt = attempt + 1
+      }
     } while (!atomicCounters.compareAndSet(currentCounter, newCounter))
     val current = currentCounter
     current
   }
 
+  /**
+   * updates the atomicCounters with an additional time measurement
+   *
+   * the method uses an atomic with lock update pattern
+   * we attempt to update the atomicCounters variable.
+   * If we fail due to concurrency, we try again, but this time
+   * we lock to ensure no one interrupts and updates our atomicCounters in the middle.
+   * @param newState the new state the counter gets into
+   * @param hasError if an error was ancountered and should be counted
+   */
   private def flushState(newState: State.State, hasError: Boolean) {
     val now = timeSource()
     var counters: Counters = null
     var newCounters: Counters = null
+    var attempt = 1
     do {
-      counters = atomicCounters.get
-      var overheadTime = counters.overheadTime
-      var sleepTime = counters.sleepTime
-      var workTime = counters.workTime
-      var errorCount = counters.errorCount
-      counters.state match {
-        case State.overhead => overheadTime = overheadTime + (now-counters.lastStateChange)
-        case State.sleeping => sleepTime = sleepTime + (now-counters.lastStateChange)
-        case State.working => workTime = workTime + (now-counters.lastStateChange)
+      lockOnSubsequentAttempt(attempt)
+      try {
+        counters = atomicCounters.get
+        var overheadTime = counters.overheadTime
+        var sleepTime = counters.sleepTime
+        var workTime = counters.workTime
+        var errorCount = counters.errorCount
+        counters.state match {
+          case State.overhead => overheadTime = overheadTime + (now-counters.lastStateChange)
+          case State.sleeping => sleepTime = sleepTime + (now-counters.lastStateChange)
+          case State.working => workTime = workTime + (now-counters.lastStateChange)
+        }
+        if (hasError)
+          errorCount = errorCount + 1
+        newCounters = Counters(overheadTime, sleepTime, workTime, errorCount, newState, timeSource())
       }
-      if (hasError)
-        errorCount = errorCount + 1
-      newCounters = Counters(overheadTime, sleepTime, workTime, errorCount, newState, timeSource())
+      finally {
+        releaseOnSubsequentAttempt(attempt)
+        attempt = attempt + 1
+      }
     } while (!atomicCounters.compareAndSet(counters, newCounters))
+  }
+
+  private def lockOnSubsequentAttempt(attempt: Int) {
+    if (attempt > 1)
+      lock.lock()
+  }
+
+  private def releaseOnSubsequentAttempt(attempt: Int) {
+    if (attempt > 1)
+      lock.unlock()
   }
 
   private[ConnectionWorkerMeter] object State extends Enumeration {
@@ -95,12 +141,12 @@ class ConnectionWorkerMeter(val timeSource: () => Millis = () => System.currentT
   }
 
   private[ConnectionWorkerMeter] case class Counters(overheadTime: Millis = 0,
-                      sleepTime: Millis = 0,
-                      workTime: Millis = 0,
-                      errorCount: Int = 0,
-                      state: State.State = State.overhead,
-                      lastStateChange: Millis = timeSource()
-                       )
+                                                     sleepTime: Millis = 0,
+                                                     workTime: Millis = 0,
+                                                     errorCount: Int = 0,
+                                                     state: State.State = State.overhead,
+                                                     lastStateChange: Millis = timeSource()
+                                                      )
 
 }
 
