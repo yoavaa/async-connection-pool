cc3bb0c88ac8cc31f47b221f7764acfb438dee24
Yoav Abrahami <yoav@wix.com>
2013-05-05 19:11:54 +0300

    acceptance test

--- src/test/scala/com/wixpress/hoopoe/asyncjdbc/impl/AcceptenceTest.scala
+++ src/test/scala/com/wixpress/hoopoe/asyncjdbc/impl/AcceptenceTest.scala
@@ -2,12 +2,14 @@ package com.wixpress.hoopoe.asyncjdbc.impl
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
-import com.wixpress.hoopoe.asyncjdbc.QueuedDataSource
+import com.wixpress.hoopoe.asyncjdbc.{AsyncDataSource, QueuedDataSource}
 import concurrent.{Await, Future}
 import concurrent.duration.Duration
 import java.util.concurrent.{LinkedBlockingQueue, BlockingQueue, TimeUnit}
 import util.Success
 import java.sql.Connection
+import collection.mutable.ArrayBuffer
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  *
@@ -18,51 +20,40 @@ class AcceptenceTest extends FlatSpec with ShouldMatchers {
 
 
   "QueuedDataSource" should "execute sql" in withDataSource { datasource =>
-    val res: Future[Boolean] = datasource.doWithConnection(conn => {
+    val res: Future[Unit] = datasource.doWithConnection(conn => {
       execSql(conn, "create table mytable (a int, b varchar(10))")
-      true
     })
     Await.ready(res, Duration(100, TimeUnit.SECONDS))
-    res.value should equal(Some(Success(true)))
+    res.value should equal(Some(Success()))
   }
 
   it should "execute subsequent sql" in withDataSource { datasource =>
     createMeTable(datasource)
 
-    val res2: Future[Boolean] = datasource.doWithConnection(conn => {
+    val res2: Future[Unit] = datasource.doWithConnection(conn => {
       execSql(conn, "insert into mytable (a,b) values (1, 'abc')")
-      true
     })
     Await.ready(res2, Duration(100, TimeUnit.SECONDS))
-    res2.value should equal(Some(Success(true)))
+    res2.value should equal(Some(Success()))
   }
 
 
   it should "execute 2 sqls in parallel" in withDataSource { datasource =>
     createMeTable(datasource)
 
-    val res1: Future[Boolean] = datasource.doWithConnection(conn => {
-      execSql(conn, "insert into mytable (a,b) values (1, 'abc')")
-      true
-    })
-    val res2: Future[Boolean] = datasource.doWithConnection(conn => {
-      execSql(conn, "insert into mytable (a,b) values (2, 'def')")
-      true
-    })
+    val res1 = insert(datasource, 1, "abc")
+    val res2 = insert(datasource, 2, "def")
     Await.ready(res1, Duration(100, TimeUnit.SECONDS))
     Await.ready(res2, Duration(100, TimeUnit.SECONDS))
-    res1.value should equal(Some(Success(true)))
-    res2.value should equal(Some(Success(true)))
+    res1.value should equal(Some(Success()))
+    res2.value should equal(Some(Success()))
   }
 
   it should "returned queried values" in withDataSource { datasource =>
     createMeTable(datasource)
+    val res = insert(datasource, 1, "abc")
+    Await.ready(res, Duration(100, TimeUnit.SECONDS))
 
-    val res1: Future[Boolean] = datasource.doWithConnection(conn => {
-      execSql(conn, "insert into mytable (a,b) values (1, 'abc')")
-      true
-    })
-    Await.ready(res1, Duration(100, TimeUnit.SECONDS))
     val res2: Future[Option[String]] = datasource.doWithConnection(conn => {
       val ps = conn.prepareStatement("select * from mytable where a = 1")
       try {
@@ -85,7 +76,7 @@ class AcceptenceTest extends FlatSpec with ShouldMatchers {
     res2.value should equal(Some(Success(Some("abc"))))
   }
 
-  def createMeTable(datasource: QueuedDataSource) {
+  def createMeTable(datasource: AsyncDataSource) {
     val res: Future[Boolean] = datasource.doWithConnection(conn => {
       execSql(conn, "create table mytable (a int, b varchar(10))")
       true
@@ -94,6 +85,42 @@ class AcceptenceTest extends FlatSpec with ShouldMatchers {
     res.value should equal(Some(Success(true)))
   }
 
+  def insert(datasource: AsyncDataSource, index: Int, value: String): Future[Unit] = {
+    datasource.doWithConnection(conn => {
+      val ps = conn.prepareStatement("insert into mytable (a,b) values (?, ?)")
+      try {
+        ps.setInt(1, index)
+        ps.setString(2, value)
+        ps.execute()
+        println(index)
+      }
+      finally {
+        ps.close()
+      }
+    })
+  }
+
+  def select(datasource: AsyncDataSource, index: Int): Future[Option[String]] = {
+    datasource.doWithConnection(conn => {
+      val ps = conn.prepareStatement("select * from mytable where a = ?")
+      try {
+        ps.setInt(1, index)
+        val rs = ps.executeQuery()
+        try {
+        if (rs.next)
+          Some(rs.getString("b"))
+        else
+          None
+        }
+        finally {
+          rs.close()
+        }
+      }
+      finally {
+        ps.close()
+      }
+    })
+  }
 
   def execSql(conn: Connection, sql: String) {
     val statement = conn.prepareStatement(sql)
@@ -115,6 +142,12 @@ class AcceptenceTest extends FlatSpec with ShouldMatchers {
     }
   }
 
+  class WorkflowStats(val start: Long = System.nanoTime(),
+                      val sqls:ArrayBuffer[Long] = new ArrayBuffer[Long]) {
+    def print {
+      println("%d, %d,%d,%d,%d".format(start, sqls(0), sqls(1), sqls(2), sqls(3)))
+    }
+  }
 
 
 }
